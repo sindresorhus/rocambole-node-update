6174b4c5b8934a47bf22cbfe86016c973b7b578d
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-22 16:23:38 +0100

    init

---
+++ test.js
@@ -0,0 +1,14 @@
+'use strict';
+var assert = require('assert');
+var rocambole = require('rocambole');
+var updateNode = require('./index');
+
+it('should update a AST node', function () {
+	var str = rocambole.moonwalk('if (true) { foo() }', function (node) {
+		if (node.type === 'CallExpression') {
+			updateNode(node, 'bar()');
+		}
+	}).toString();
+
+	assert.strictEqual(str, 'if (true) { bar() }');
+});
